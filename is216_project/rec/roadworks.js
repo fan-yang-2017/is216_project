7d4bb7568a8dcc0552bc58fe6517416c5d6a4c12
Tan Jia Ruey <jordonjr97@gmail.com>
2020-10-25 11:58:35 +0800

    roadworks API & JS

---
+++ is216_project/rec/roadworks.js
@@ -0,0 +1,21 @@
+
+
+function getRoadworks() {
+    url ="roadworksAPI.php";
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // var data = JSON.parse(this.responseText);
+            // document.write(this.responseText);
+            var json_obj = JSON.parse(this.responseText);
+            var roadworkrecords = json_obj.value;
+
+            // E.g extracting the roadname from the json obj. 
+            console.log(json_obj.value[0].RoadName);
+            
+        }
+    }
+    request.open("GET", url, true);
+    request.send();
+}
