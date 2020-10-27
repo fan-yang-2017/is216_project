264ea321317e8a7344b15d987a20a3e8e3b824ba
Tan Jia Ruey <jordonjr97@gmail.com>
2020-10-27 13:46:06 +0800

    API for bus stop code and name

---
+++ is216_project/rec/busstop.js
@@ -0,0 +1,23 @@
+
+
+function getBusStop() {
+    url ="busStopAPI.php";
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // var data = JSON.parse(this.responseText);
+            // document.write(this.responseText);
+
+            var json_obj = JSON.parse(this.responseText);
+            var records = json_obj.value;
+
+            // E.g extracting the bus service no. from the json obj. 
+            console.log(records[0].BusStopCode);
+            console.log(records[0].Description);
+            
+        }
+    }
+    request.open("GET", url, true);
+    request.send();
+}
