2b545f1ded6ab0f0f8710c9568f55e5c392a9874
Tan Jia Ruey <jordonjr97@gmail.com>
2020-11-06 22:52:13 +0800

    changed type from submit to button

--- is216_project/rec/bus_arrival.js
+++ is216_project/rec/bus_arrival.js
@@ -3,7 +3,7 @@ function sendFormData(){
     var formData = new FormData();
     // console.log(formData);
     var bs_code = document.getElementById("bs_code").value;
-    console.log(bs_code);
+    console.log(typeof(bs_code));
     formData.append("bs_code",bs_code); 
     // for(var pair of formData.entries()) {
     //     console.log(pair[0]+', '+pair[1]);
