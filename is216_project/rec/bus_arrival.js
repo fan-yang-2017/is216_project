a03399eadf0abf0e274f4e3d5804389ee354f136
Tan Jia Ruey <jordonjr97@gmail.com>
2020-11-07 02:07:41 +0800

    left bus time in mins, dynamic arrival display

--- is216_project/rec/bus_arrival.js
+++ is216_project/rec/bus_arrival.js
@@ -3,7 +3,10 @@ function sendFormData(){
     var formData = new FormData();
     // console.log(formData);
     var bs_code = document.getElementById("bs_code").value;
-    console.log(typeof(bs_code));
+    // console.log(bs_code);
+
+    var bus_display = document.getElementById("bus_display");
+
     formData.append("bs_code",bs_code); 
     // for(var pair of formData.entries()) {
     //     console.log(pair[0]+', '+pair[1]);
@@ -13,8 +16,31 @@ function sendFormData(){
     var request = new XMLHttpRequest();
 
     request.onload = function(){
-        var data = JSON.parse(this.responseText);
-        console.log(data);
+        // console.log(JSON.parse(this.responseText));
+        var data = JSON.parse(this.responseText).Services;
+        // console.log(data);
+
+        //clearing the options when searching for different postal codes
+        while (bus_display.lastChild !== null) {;
+            bus_display.lastChild.remove();
+        }
+            
+        if (bus_display !== null ) {
+            div2.classList.remove("d-none");
+        }
+
+        for (var i of data) {
+            var option = document.createElement("option");
+
+            option.setAttribute("value", i.ServiceNo);
+
+            var textNode = document.createTextNode(i.ServiceNo);
+
+            option.appendChild(textNode);
+
+            bus_display.appendChild(option);
+
+        }
     }
     
     request.open("POST", url, true);
@@ -22,6 +48,55 @@ function sendFormData(){
 
     // getBusArrival();
 }
+
+function getBusArrival(busNo){
+    var bus = busNo.options[busNo.selectedIndex].text;
+    console.log(bus);
+    
+    //sending data to php file
+    var formData = new FormData();
+    // console.log(formData);
+    var bs_code = document.getElementById("bs_code").value;
+    // console.log(bs_code);
+
+    formData.append("bs_code",bs_code); 
+    // for(var pair of formData.entries()) {
+    //     console.log(pair[0]+', '+pair[1]);
+    //   }
+
+    var url = "../rec/busArrivalAPI.php";
+    var request = new XMLHttpRequest();
+
+    request.onload = function(){
+        // console.log(JSON.parse(this.responseText));
+        var data = JSON.parse(this.responseText).Services;
+        console.log(data);
+        var d = new Date();
+        var n = d.getMinutes();
+
+        // var arrival_time = document.getElementById("bus_arrival");
+        var output = "";
+
+        for (i of data) {
+            if (i.ServiceNo == bus) {
+                var nextbus = i.NextBus.EstimatedArrival.getMinutes() - n;
+                var nextbus2 = i.NextBus2.EstimatedArrival.getMinutes() - n;
+                output += "<tr>";
+                output += "<td>" + i.ServiceNo + "</td>";
+                output += "<td>" + nextbus + "</td>";
+                output += "<td>" + nextbus2 + "</td>";
+                output += "</tr>"
+
+                document.getElementById("arrival_time").innerHTML = output;
+            }
+        }
+
+    }
+
+    request.open("POST", url, true);
+    request.send(formData);
+}
+
 
 // function getBusArrival(){
 //     //receiving data 
