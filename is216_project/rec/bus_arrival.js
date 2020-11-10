906fd6803fc8045219f79153396ec9acc49161e7
Tan Jia Ruey <jordonjr97@gmail.com>
2020-11-10 14:02:38 +0800

    all bus display done, deleted roadwork btn mrt pg

--- is216_project/rec/bus_arrival.js
+++ is216_project/rec/bus_arrival.js
@@ -29,6 +29,12 @@ function sendFormData(){
             div2.classList.remove("d-none");
         }
 
+        var op = document.createElement("option");
+        op.setAttribute("value", "all");
+        var text = document.createTextNode("All buses");
+        op.appendChild(text);
+        bus_display.appendChild(op);
+
         for (var i of data) {
             var option = document.createElement("option");
 
@@ -90,7 +96,58 @@ function getBusArrival(busNo){
                 // var nextbus3 = new Date(rawNext3Date);
 
                 // bus service info check
-                console.log(i);
+                // console.log(i);
+
+                // covert from miliseconds to seconds to minutes
+                var nextBusTime = Math.floor(((nextbus.getTime() - d.getTime())/1000)/60);
+                var nextBus2Time = Math.floor(((nextbus2.getTime() - d.getTime())/1000)/60);
+                // var nextBus3Time = Math.floor(((nextbus3.getTime() - d.getTime())/1000)/60);
+
+                output += "<tr>";
+                output += "<td>" + i.ServiceNo + "</td>";
+
+                // fix bug if no more bus services
+                if (rawNextDate !== "") {
+                    if (nextBusTime <= 0) {
+                        output += "<td>Arriving</td>";
+                    }
+                    else {
+                        output += "<td>" + nextBusTime + " mins</td>";
+                    }
+                }
+                else {
+                    output += "<td>-</td>";
+                }
+
+                if (rawNext2Date !== "") {
+                    if (nextBus2Time <= 0) {
+                        output += "<td>Arriving</td>";
+                    } 
+                    else {
+                        output += "<td>" + nextBus2Time + " mins</td>";
+                    }
+                }
+                else {
+                    output += "<td>-</td>";
+                }
+                    
+                output += "</tr>"
+
+                document.getElementById("arrival_time").innerHTML = output;
+            }
+            else if (bus == "All buses") {
+                var rawNextDate = i.NextBus.EstimatedArrival;
+                var rawNext2Date = i.NextBus2.EstimatedArrival;
+                // var rawNext3Date = i.NextBus3.EstimatedArrival;
+
+                // set retrieved time into javascript date format
+                var nextbus = new Date(rawNextDate);
+                var nextbus2 = new Date(rawNext2Date);
+
+                // var nextbus3 = new Date(rawNext3Date);
+
+                // bus service info check
+                // console.log(i);
 
                 // covert from miliseconds to seconds to minutes
                 var nextBusTime = Math.floor(((nextbus.getTime() - d.getTime())/1000)/60);
