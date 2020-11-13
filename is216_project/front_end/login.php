cc5a829f6b5dbbb54ae61ed9b143486937264bed
Zq619 <qi.zhao.2019@smu.edu.sg>
2020-11-13 22:45:38 +0800

    credit to log in page css

--- is216_project/front_end/login.php
+++ is216_project/front_end/login.php
@@ -17,7 +17,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
         <title>Time Traveller - Login</title>
-
+        <!-- /* credits to https://getbootstrap.com/docs/4.0/examples/ for login page */-->
         <style>
             .bd-placeholder-img {
                 font-size: 1.125rem;
