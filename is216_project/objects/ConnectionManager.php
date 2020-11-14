2702028cb3e5d334e4c65603b3352ee74e1be770
yuhaoN <yuhao.neo.2019@sis.smu.edu.sg>
2020-11-14 19:20:31 +0800

    update

--- is216_project/objects/ConnectionManager.php
+++ is216_project/objects/ConnectionManager.php
@@ -1,10 +1,10 @@
 <?php
     class ConnectionManager {
         public function getConnection() {   
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "user";
+            $servername = "us-cdbr-east-02.cleardb.com";
+            $username = "bb1426d54f2b72";
+            $password = "8d86cbac";
+            $dbname = "heroku_29de17d9730b345";
             $port = "3306";
             
             // Create connection
